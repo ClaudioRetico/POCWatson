0dfb34ba8e84a14cf3a5bfff9f764c5c93cea750
IT040213 <IT040213@IBM051-PBRB46C>
2017-09-13 17:23:18 +0200

    Ulteriori estrazioni documenti

--- DarkInfoEstraction_PV_POC/src/sqlscriptvari/EstraiDoc_secondoEstratto.sql
+++ DarkInfoEstraction_PV_POC/src/sqlscriptvari/EstraiDoc_secondoEstratto.sql
@@ -6,13 +6,15 @@ FROM DASH13540."PA_PV_COMPLETE"
 WHERE "Case ID" IN (SELECT "Case ID" AS NBR
                       FROM DASH13540."PA_PV_COMPLETE"
                       WHERE LENGTH("Note for answer to client") BETWEEN 1 AND 100
-                      FETCH FIRST 30 ROWS ONLY)
+                        AND "Case ID" IN (SELECT ID FROM DASH13540."ID_ESCLUSI") 
+                      FETCH FIRST 30 ROWS ONLY)              
 UNION ALL     
 SELECT "Case ID","Note for answer to client"
 FROM DASH13540."PA_PV_COMPLETE" 
 WHERE "Case ID" IN (SELECT "Case ID" AS NBR
                       FROM DASH13540."PA_PV_COMPLETE"
                       WHERE LENGTH("Note for answer to client") BETWEEN 101 AND 200
+                        AND "Case ID" IN (SELECT ID FROM DASH13540."ID_ESCLUSI") 
                       FETCH FIRST 30 ROWS ONLY)
 UNION ALL                      
 SELECT "Case ID","Note for answer to client"
@@ -20,6 +22,7 @@ FROM DASH13540."PA_PV_COMPLETE"
 WHERE "Case ID" IN (SELECT "Case ID" AS NBR
                       FROM DASH13540."PA_PV_COMPLETE"
                       WHERE LENGTH("Note for answer to client") BETWEEN 201 AND 300
+                      AND "Case ID" IN (SELECT ID FROM DASH13540."ID_ESCLUSI") 
                       FETCH FIRST 20 ROWS ONLY) 
 UNION ALL
 SELECT "Case ID","Note for answer to client"
@@ -27,6 +30,7 @@ FROM DASH13540."PA_PV_COMPLETE"
 WHERE "Case ID" IN (SELECT "Case ID" AS NBR
                       FROM DASH13540."PA_PV_COMPLETE"
                       WHERE LENGTH("Note for answer to client") BETWEEN 301 AND 400
+                      AND "Case ID" IN (SELECT ID FROM DASH13540."ID_ESCLUSI") 
                       FETCH FIRST 10 ROWS ONLY)    
 UNION ALL                      
 SELECT "Case ID","Note for answer to client"
@@ -34,6 +38,7 @@ FROM DASH13540."PA_PV_COMPLETE"
 WHERE "Case ID" IN (SELECT "Case ID" AS NBR
                       FROM DASH13540."PA_PV_COMPLETE"
                       WHERE LENGTH("Note for answer to client") BETWEEN 400 AND 500
+                      AND "Case ID" IN (SELECT ID FROM DASH13540."ID_ESCLUSI") 
                       FETCH FIRST 10 ROWS ONLY)
 UNION ALL                      
 SELECT "Case ID","Note for answer to client"
@@ -41,6 +46,7 @@ FROM DASH13540."PA_PV_COMPLETE"
 WHERE "Case ID" IN (SELECT "Case ID" AS NBR
                       FROM DASH13540."PA_PV_COMPLETE"
                       WHERE LENGTH("Note for answer to client") BETWEEN 1000 AND 1100
+                      AND "Case ID" IN (SELECT ID FROM DASH13540."ID_ESCLUSI") 
                       FETCH FIRST 15 ROWS ONLY)                      
 UNION ALL                      
 SELECT "Case ID","Note for answer to client"
@@ -48,6 +54,7 @@ FROM DASH13540."PA_PV_COMPLETE"
 WHERE "Case ID" IN (SELECT "Case ID" AS NBR
                       FROM DASH13540."PA_PV_COMPLETE"
                       WHERE LENGTH("Note for answer to client") BETWEEN 3000 AND 4000
+                      AND "Case ID" IN (SELECT ID FROM DASH13540."ID_ESCLUSI") 
                       FETCH FIRST 15 ROWS ONLY)                      
                       
                       )                    
